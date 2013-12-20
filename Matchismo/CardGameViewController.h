7f0b3836d6c477bc0f7ad0d6637e15f4ca83f5fa
Chris Tibbs <christibbs@Chriss-MacBook-Air.local>
2013-12-20 13:22:19 -0800

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Chris Tibbs on 12/20/13.
+//  Copyright (c) 2013 Chris Tibbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
